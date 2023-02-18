bfb30845a58fe3a9bcdcfde6345a7ed56650c580
Nehal <mdnehaladil@gmail.com>
2023-02-18 14:00:42 +0530

    use Hooks to Submit a Post

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
+
 function CreatePost() {
+
+    const [title, setTitle] = useState();
+    const [subTitle, setSubTitle] = useState();
+    const [content, setContent] = useState();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log("Title: ", title);
+        console.log("SubTitle: ", subTitle);
+        console.log("Content: ", content);
+    }
+
     return (
         <div className="create-post">
             <h1>Create Post</h1>
 
-            <form >
+            <form onSubmit={handleSubmit}>
                 
                 <div className="form-field">
                     <label>Title</label>
-                    <input />
+                    <input value={title} onChange={(e)=> setTitle(e.target.value)} />
                 </div>
                 
                 <div className="form-field">
                     <label>Sub Title</label>
-                    <input />
+                    <input value={subTitle} onChange={(e)=> setSubTitle(e.target.value)} />
                 </div>
                 
                 <div className="form-field">
                     <label>Content</label>
-                    <textarea></textarea>
+                    <textarea value={content} onChange={(e)=> setContent(e.target.value)}></textarea>
                 </div>
 
                 <button className="create-post-btn">Create Post</button>
