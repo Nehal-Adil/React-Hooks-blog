86c09c9e63ba836abc37e6acdd3e7553a2b2c3ea
Nehal <mdnehaladil@gmail.com>
2023-02-18 16:35:23 +0530

    add postDetails using useEffect and firestore(snapshot)

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { firestore } from "../Firebase";
+import {useParams} from 'react-router-dom';
+
 function PostDetail() {
+    const [post, setPost] = useState({});
+    const {postId} = useParams();
+
+    useEffect(()=> {
+        firestore
+            .collection('posts')
+            .doc(postId)
+            .get()
+            .then((snapshot)=> {
+            console.log("Snapshot: ", snapshot.data);
+            setPost(snapshot.data());    
+        });
+    }, []);
+
     return (
-        <div className="">
-            PostDetail
+        <div className="post-detail">
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
         </div>
     )
 }
