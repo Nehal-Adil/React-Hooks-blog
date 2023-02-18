f90dd1e678a7fc5026d65886598e77581ac67a1b
Nehal <mdnehaladil@gmail.com>
2023-02-18 16:26:55 +0530

    Implemeting Home with useEffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import { firestore } from "../Firebase";
+
 function Home() {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        firestore
+            .collection('posts')
+            .get()
+            .then((snapshot) => {
+                const posts = snapshot.docs.map((doc) => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                }
+            });
+
+            console.log("Posts: ", posts);
+            setPosts(posts);
+        });
+    }, []);
+    
     return (
-        <div className="">
-            <h3>Home</h3>
+        <div className="home">
+          <h1>Tech Blog</h1>
+          <div id="blog-by">Coding Ninjas</div>
+    
+          {posts.map((post, index) => (
+            <div className="post" key={`post-${index}`}>
+              <Link to={`/post/${post.id}`}>
+                <h3>{post.title}</h3>
+              </Link>
+    
+              <p>{post.subTitle}</p>
+            </div>
+          ))}
         </div>
-    )
+      );
 }
 
 export default Home;
