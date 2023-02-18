d50e0ad0203837cc0b2b7bd74327ddeba1800487
Nehal <mdnehaladil@gmail.com>
2023-02-18 13:52:02 +0530

    add create-post page and style it

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,7 +1,27 @@
 function CreatePost() {
     return (
-        <div className="">
+        <div className="create-post">
             <h1>Create Post</h1>
+
+            <form >
+                
+                <div className="form-field">
+                    <label>Title</label>
+                    <input />
+                </div>
+                
+                <div className="form-field">
+                    <label>Sub Title</label>
+                    <input />
+                </div>
+                
+                <div className="form-field">
+                    <label>Content</label>
+                    <textarea></textarea>
+                </div>
+
+                <button className="create-post-btn">Create Post</button>
+            </form>
         </div>
     )
 }
